5326456108014f36e95128801b1904148b4a6747
2nd-Runner <2nd.runnr@gmail.com>
2023-06-07 12:09:21 -0700

    connect signin post to db

--- server.js
+++ server.js
@@ -22,14 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.options('/products/:id', cors())
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-app.del('/products/:id', cors(), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-app.get('/', (req, res) => { res.send('it is working!') })
+app.get('/', (req, res) => { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
