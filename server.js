3bde12af80c70437cbee0db08d8d117df1751e0d
2nd-Runner <2nd.runnr@gmail.com>
2023-06-07 10:08:58 -0700

    cors preflight enable

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.options('/products/:id', cors())
 app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+app.del('/products/:id', cors(), function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 app.get('/', (req, res) => { res.send('it is working!') })
