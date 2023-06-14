9e132212f025df509e4d865c4944c6366770bc8a
2nd-Runner <2nd.runnr@gmail.com>
2023-06-13 21:01:12 -0700

    pg updated to latest, fixing bug connection timeout bug

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -17,7 +18,21 @@ const db = knex({
   }
 });
 
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host : '127.0.0.1',
+//     port : 5432,
+//     user : 'am',
+//     password : '',
+//     database : 'smart-brains'
+//     }
+// });
 
+// console.log(db.select('*').from('users'));
+// db.select('*').from('users').then(data => {
+//   console.log(data);
+// });
 
 const app = express();
 
@@ -32,6 +47,11 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
+
+
+// app.listen(3000, () => {
+//   console.log(`app is running on port 3000`);
+// })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
